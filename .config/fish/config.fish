205d93bb282e05d7de09a4351e6daf01239ebe68
Nick Miller <nick@nmiller.info>
2020-04-08 13:52:21 -0400

    Mutate $fish_user_paths instead of $PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,24 +1,29 @@
 set -x GOPATH $HOME/Projects/go
-set PATH /usr/local/opt/curl/bin $PATH
-set PATH /usr/local/opt/coreutils/libexec/gnubin /usr/local/sbin $PATH
-set PATH $PATH $GOPATH/bin
+
+set fish_user_paths /usr/local/opt/curl/bin $fish_user_paths
+set fish_user_paths /usr/local/opt/coreutils/libexec/gnubin $fish_user_paths
+set fish_user_paths /usr/local/sbin $fish_user_paths
+set fish_user_paths /usr/local/opt/ruby/bin $fish_user_paths
+set fish_user_paths $fish_user_paths $GOPATH/bin
 
 # Environment variables:
 set -xg EDITOR (which nvim)
 set -xU LS_COLORS 'di=1'
 
 # pure:
-set -xU pure_color_mute (set_color brcyan)
-set -xU pure_color_success (set_color normal)
-set -xU pure_symbol_prompt '>'
+set pure_color_mute brcyan
+set pure_color_success normal
+set pure_color_command_duration pure_color_mute
+set pure_symbol_prompt '>'
 
 # Wine:
 set -x FREETYPE_PROPERTIES truetype:interpreter-version=35
 
 # Aliases:
+alias config='git --git-dir=$HOME/.cfg --work-tree=$HOME'
 alias ls='ls -ohAH --color --group-directories-first'
-alias vim='nvim'
-alias vi='nvim'
 alias p='set-profile'
 alias t='set-title'
-alias config='git --git-dir=$HOME/.cfg --work-tree=$HOME'
+alias vim='$EDITOR'
+alias vi='$EDITOR'
+alias e='$EDITOR'
